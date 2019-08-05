56449960db23e5d8e5ade01505975ff4072fb9e5
hcrobert <c.habimana@irembo.com>
2019-08-05 22:29:22 +0200

    fixed the conflict

--- src/main/java/rw/ehealth/controller/ReportController.java
+++ src/main/java/rw/ehealth/controller/ReportController.java
@@ -1,62 +1,30 @@
-<<<<<<< HEAD
 
-package rw.ehealth.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import rw.ehealth.model.AdmissionInfo;
-import rw.ehealth.service.admission.IAdmissionService;
-
-@RestController
-@RequestMapping("/report")
-public class ReportController {
-
-	@Autowired
-	private IAdmissionService admissionService;
-
-	@PostMapping("/admission/month")
-	public ResponseEntity<List<AdmissionInfo>> getAdmisionsReport(@RequestParam int month) {
-		System.out.println(month + " the month we're looking for");
-		List<AdmissionInfo> results = admissionService.getAdmissionsPerMonth(month);
-		return new ResponseEntity<>(results, HttpStatus.OK);
-	}
-
-=======
 package rw.ehealth.controller;
 
 import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import rw.ehealth.model.AdmissionInfo;
 import rw.ehealth.model.Doctor;
-import rw.ehealth.service.admission.AdmissionService;
-import rw.ehealth.service.medical.HospitalService;
-import rw.ehealth.service.patient.PatientService;
+import rw.ehealth.service.admission.IAdmissionService;
 import rw.ehealth.service.user.UserService;
 
 @Controller
 public class ReportController {
 	@Autowired
+	private IAdmissionService admissionService;
+	@Autowired
 	private UserService userService;
-	@Autowired
-	private HospitalService hospitalService;
-	@Autowired
-	private PatientService patientservice;
-	@Autowired
-	private AdmissionService admissionService;
+
 	@GetMapping("/reception/report")
 	public String Receptionist(Model model, Principal principal) {
 		String username = principal.getName();
@@ -66,16 +34,24 @@ public class ReportController {
 		model.addAttribute("admission", admissions);
 		return "report";
 	}
+
 	@GetMapping("/reception/report/male")
 	public String getAdmissionByGender(Model model, Principal principal) {
 		String username = principal.getName();
 		Doctor doctor = userService.findDoctor(username);
 		boolean gender = true;
-		model.addAttribute("gender",gender);
+		model.addAttribute("gender", gender);
 		List<AdmissionInfo> admissions = admissionService.findByGender("MALE");
 		model.addAttribute("doctor", doctor);
 		model.addAttribute("maleAdmision", admissions);
 		return "report";
 	}
->>>>>>> dc87a1d13edcf3ef8b928bfeea08f0964ecd2f95
+
+	@PostMapping("/report/admission/month")
+	public ResponseEntity<List<AdmissionInfo>> getAdmisionsReport(@RequestParam int month) {
+		System.out.println(month + " the month we're looking for");
+		List<AdmissionInfo> results = admissionService.getAdmissionsPerMonth(month);
+		return new ResponseEntity<>(results, HttpStatus.OK);
+
+	}
 }
