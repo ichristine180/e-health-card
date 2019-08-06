d774ea975af74911645de88d959a5a5c72151e04
ichristine180 <ichristine180@gmail.com>
2019-08-06 14:23:00 +0200

    bbb

--- src/main/java/rw/ehealth/controller/ReportController.java
+++ src/main/java/rw/ehealth/controller/ReportController.java
@@ -17,8 +17,6 @@ import rw.ehealth.service.admission.AdmissionService;
 import rw.ehealth.service.medical.HospitalService;
 import rw.ehealth.service.patient.PatientService;
 import rw.ehealth.service.user.UserService;
-import rw.ehealth.utils.AdmissionReportData;
-import rw.ehealth.utils.IndividualReportData;
 import rw.ehealth.utils.PinfoListResponse;
 
 @Controller
@@ -43,19 +41,17 @@ public class ReportController {
 	
 	@GetMapping("/reception/reports/male")
 	public String getAdmissionByGender(Model model, Principal principal) {
-		IndividualReportData response = new IndividualReportData();
 		String username = principal.getName();
 		Doctor doctor = userService.findDoctor(username);
 		boolean gender = true;
 		model.addAttribute("gender",gender);
 		List<AdmissionInfo> results = admissionService.findByGender("MALE");
-			response.setAdmissionInfos(results);
-
+			
 			List<AdmissionInfo> admission = new ArrayList<AdmissionInfo>();
 			admission.addAll(results);
-			response.setAdmissionInfos(admission);
+			
 		model.addAttribute("doctor", doctor);
-		model.addAttribute("maleAdmision", response);
+		model.addAttribute("maleAdmision", results);
 		return "report";
 	}
 	@GetMapping("/admission/month")
