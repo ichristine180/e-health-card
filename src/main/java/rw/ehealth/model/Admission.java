09b33f13f1a54bca587b0f8e289d1a23bd0a0b6b
ichristine180 <ichristine180@gmail.com>
2019-09-07 11:05:41 +0200

    www

--- src/main/java/rw/ehealth/model/Admission.java
+++ src/main/java/rw/ehealth/model/Admission.java
@@ -1,7 +1,8 @@
 
 package rw.ehealth.model;
 
-import javax.persistence.Column;
+import javax.persistence.Column
+;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,10 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.NotBlank;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- src/test/java/rw/ehealth/EHealthCardApplicationTests.java
+++ src/test/java/rw/ehealth/EHealthCardApplicationTests.java
@@ -1,5 +1,9 @@
 package rw.ehealth;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,10 +12,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import rw.ehealth.model.Admission;
+import rw.ehealth.model.Department;
+import rw.ehealth.model.Employee;
 import rw.ehealth.model.Hospital;
 import rw.ehealth.model.Patient;
+import rw.ehealth.model.User;
+import rw.ehealth.model.security.Role;
+import rw.ehealth.model.security.UserRole;
+import rw.ehealth.service.admission.IAdmissionService;
 import rw.ehealth.service.hospital.IHospitalService;
 import rw.ehealth.service.patient.IPatientService;
+import rw.ehealth.service.user.DepartemtService;
+import rw.ehealth.service.user.IUserService;
 import rw.ehealth.utils.IDGenerator;
 
 @RunWith(SpringRunner.class)
@@ -23,17 +36,21 @@ public class EHealthCardApplicationTests {
 	private IHospitalService hospitalService;
 
 	@Autowired
+	private IUserService userService;
+	@Autowired
 	private IPatientService patientService;
-
+	@Autowired
+	private DepartemtService departemtService;
+	Hospital createdHospital;
 	@Test
 	public void testCreateHospital() {
 
 		Hospital hospital = new Hospital();
-		hospital.setAddress("Ndera");
-		hospital.setHospitalCode("HVP");
-		hospital.setHospitalName("Ndera-Mu Mutwe");
+		hospital.setAddress("Nyamata");
+		hospital.setHospitalCode("Hn");
+		hospital.setHospitalName("Hopital_Nyamata");
 
-		Hospital createdHospital = hospitalService.createHospital(hospital);
+	createdHospital = hospitalService.createHospital(hospital);
 
 		Assert.assertEquals(hospital.getHospitalName(), createdHospital.getHospitalName());
 
@@ -50,12 +67,36 @@ public class EHealthCardApplicationTests {
 		patient.setLname("Mukamana");
 		patient.setGender("F");
 		patient.setHospital("KFH");
-		patient.setIdentificationNumber("1199970000852013");
+		patient.setIdentificationNumber("1234567891234567");
 
 		patient.setPatientNumber(IDGenerator.generatePatientNumber(patient));
 
 		Patient createdPatient = patientService.savePatientInfo(patient);
 		Assert.assertEquals(patient.getAddress(), createdPatient.getAddress());
+
+	}
+	@Test
+	public void testCreateDoctor() {
+		Employee doc = new Employee();
+		doc.setEmail("ichristine180@gmail.com");
+		doc.setFname("chris");
+		doc.setLname("mwiza");
+		doc.setTimestamp(LocalDate.now().toString());
+		doc.setPhone("0788028913");
+		Hospital hospitals = hospitalService.findByHospitalname("Hopital_Nyamata");
+		doc.setHospital(hospitals);
+		Department departemt = departemtService.findPerName("Laboratory");
+		doc.setDepertment(departemt);
+		User myuser = new User();
+		myuser.setUsername("ichristine180@gmail.com");
+		myuser.setPassword("pass");
+		Set<UserRole> userRoles = new HashSet<>();
+		userRoles.add(new UserRole(myuser, userService.findByName("Role_GENERAL_DOCTOR")));
+		myuser.setDoctor(doc);
+		doc.setUser(myuser);
+		userService.createUser(myuser);
+		User empoUser = userService.createUser(myuser, userRoles);
+		Assert.assertEquals(myuser.getUsername(), empoUser.getUsername());
 
 	}
 
