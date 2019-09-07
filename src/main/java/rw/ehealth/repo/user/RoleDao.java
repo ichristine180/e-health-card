352e304fb7cb14602412386a998f9c12f19de0b2
ichristine180 <ichristine180@gmail.com>
2019-09-07 15:45:10 +0200

    ee

--- src/main/java/rw/ehealth/repo/user/RoleDao.java
+++ src/main/java/rw/ehealth/repo/user/RoleDao.java
@@ -1,5 +1,8 @@
 package rw.ehealth.repo.user;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import rw.ehealth.model.security.Role;
@@ -10,5 +13,7 @@ import rw.ehealth.model.security.Role;
 
 public interface RoleDao extends CrudRepository<Role, Integer> {
 	Role findByName(String name);
+	@Query("SELECT r from Role r where r.name != 'ROLE_ADMIN'")
+	List<Role> findAll();
 	
 }
--- src/main/java/rw/ehealth/service/user/DepartemtService.java
+++ src/main/java/rw/ehealth/service/user/DepartemtService.java
@@ -1,6 +1,7 @@
 package rw.ehealth.service.user;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
