f98ea211de0fbc82b0c1c35f5b6df14e8cb688e6
ichristine180 <ichristine180@gmail.com>
2019-09-08 21:34:54 +0200

    uu

--- src/main/java/rw/ehealth/utils/PrescriptionsDto.java
+++ src/main/java/rw/ehealth/utils/PrescriptionsDto.java
@@ -1,8 +1,15 @@
 package rw.ehealth.utils;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 public class PrescriptionsDto {
+	@NotBlank(message = "please!! fill this feild is required")
+	@NotNull(message = "Please fill in some text in this text field!")
+	@Size(min = 5, max = 100, message = "this field should be  between 5 and 100 charcter!")
 	private String name;
-	private Double quantity;
 	private String patientTrackingNumber;
 	public String getPatientTrackingNumber() {
 		return patientTrackingNumber;
@@ -16,18 +23,15 @@ public class PrescriptionsDto {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Double getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(Double quantity) {
-		this.quantity = quantity;
-	}
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	@NotBlank(message = "please!! fill this feild is required")
+	@NotNull(message = "Please fill in some text in this text field!")
+	@Size(min = 5, max = 200, message = "this field should be  between 50 and 200 charcter!")
 	private String description;
 
 }
